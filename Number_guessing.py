dfbaa6b17b11f1a8e17845474ea738d213f00e73
Miguel Queiros <miguellauer010@gmail.com>
2026-04-14 17:46:31 -0300

    Number guessing Upgrade

--- Number_guessing.py
+++ Number_guessing.py
@@ -1,11 +1,19 @@
 import random
 secret_number = random.randint(1, 50)
+attempts = 0
 while True:
     guess = int(input("Type a number between 1 and 50: "))
-    if guess < secret_number:
-            print("Too low, try again.")
-    elif guess > secret_number:
-            print("Too high, try again.")
-    else:
-            print("Congratulations! You guessed the number.")
+    if guess <1 or guess > 50:
+        box = input("Invalid option. Do you want to try again? [Yes/No]: ")
+        if box.lower() == "no":
             break
+        else:
+            continue
+    attempts += 1
+    if guess < secret_number:
+        print("Too Low! Try again.")
+    elif guess > secret_number:
+        print("Too High! Try again.")
+    else:
+        print(f"\nCongratulations! You guessed the number in {attempts} attempts.")
+        break
