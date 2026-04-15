967a0b4bb4f77b1241bc2daf4b3421418629b686
Miguel Queiros <miguellauer010@gmail.com>
2026-04-15 16:53:12 -0300

    Upgrading Machine vending

--- VendingMachine.py
+++ VendingMachine.py
@@ -1,28 +1,31 @@
-cash = float(input("How much money do you have? "))
-print("[1] Soda - $2.50")
+cash = float(input("How much money do you have? $"))
+
+print("\n=== VENDING MACHINE ===")
+print("[1] Soda  - $2.50")
 print("[2] Chips - $1.75")
 print("[3] Candy - $1.25")
-print("[4] Gum - $0.50")
-option = int(input("What would you like to buy? "))
+print("[4] Gum   - $0.50")
+
+option = int(input("\nWhat would you like to buy? "))
+
 if option == 1:
-    if cash >= 2.50:
-        print("You bought a soda, enjoy!")
-    else: 
-        print("You don't have enough money for a soda.")
+    price = 2.50
+    item = "Soda"
 elif option == 2:
-    if cash >= 1.75:
-        print("You bought chips, enjoy!")
-    else:
-        print("You don't have enough money for chips.")
+    price = 1.75
+    item = "Chips"
 elif option == 3:
-    if cash >= 1.25:
-        print("You bought candy, enjoy!")
-    else:
-        print("You don't have enough money for candy.")
+    price = 1.25
+    item = "Candy"
 elif option == 4:
-    if cash >= 0.50:
-        print("You bought gum, enjoy!")
-    else:
-        print("You don't have enough money for gum.")
+    price = 0.50
+    item = "Gum"
 else:
     print("Invalid option.")
+
+if option >= 1 and option <= 4:
+    if cash >= price:
+        print(f"\nYou bought {item} for ${price:.2f}, enjoy!")
+        print(f"Your change: ${cash - price:.2f}")
+    else:
+        print(f"\nYou don't have enough money to buy {item}.")
