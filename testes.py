251c40b87ec592f1e8c7dc4097ef82d852585a8c
Miguel Queiros <miguellauer010@gmail.com>
2026-04-15 16:29:54 -0300

    Age calc upgrade

--- testes.py
+++ testes.py
@@ -1,1 +1,11 @@
-import this
+age = int(input("Type your age: "))
+
+print(f"\nYour age is: {age}")
+if age < 12:
+    print("Category: Child")
+elif age < 18:
+    print("Category: Teenager")
+elif age < 60:
+    print("Category: Adult")
+else:
+    print("Category: Elderly")
