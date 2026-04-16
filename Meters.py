db9e197536c5a360f865d6b2864a730f3c3a6e1e
Miguel Queiros <miguellauer010@gmail.com>
2026-04-16 17:57:57 -0300

    Meters, Centimeters, Milimeters

---
+++ Meters.py
@@ -0,0 +1,6 @@
+meters = float(input("Enter the amount in meters: "))
+centimeters = meters * 100
+millimeters = meters * 1000
+print(f"{meters} meters correspond to:")
+print(f"{centimeters:.0f} centimeters")
+print(f"{millimeters:.0f} millimeters")
