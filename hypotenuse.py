07b2e8ee4e4930f13b4384e2f5f3041a877015a3
Miguel Queiros <miguellauer010@gmail.com>
2026-04-17 17:15:12 -0300

    refactor: better and clean hypotenuse calc

--- hypotenuse.py
+++ hypotenuse.py
@@ -1,8 +1,10 @@
 from math import sqrt
 
-opposite = float(input("Enter the length of the opposite side: "))
-adjacent = float(input("Enter the length of the adjacent side: "))
+opposite = float(input("Enter the length of the opposite cathetus (m): "))
+adjacent = float(input("Enter the length of the adjacent cathetus (m): "))
 
 hypotenuse = sqrt(opposite ** 2 + adjacent ** 2)
 
-print(f"The opposite side is {opposite:.2f}, the adjacent side is {adjacent:.2f} and the hypotenuse is {hypotenuse:.2f}.")
+print(f"Opposite cathetus: {opposite:.2f} m")
+print(f"Adjacent cathetus: {adjacent:.2f} m")
+print(f"Hypotenuse: {hypotenuse:.2f} m")
