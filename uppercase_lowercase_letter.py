cf2e9fa9c76b444c209a30fac67fa5abeec79f09
Miguel Queiros <miguellauer010@gmail.com>
2026-04-18 15:02:05 -0300

    refactor: improving

--- uppercase_lowercase_letter.py
+++ uppercase_lowercase_letter.py
@@ -1,6 +1,11 @@
 name = input("Enter your full name: ").strip()
 
-print(name.upper())
-print(name.lower())
-print(len(name.replace(" ", "")))
-print(len(name.split()[0]))
+uppercase_name = name.upper()
+lowercase_name = name.lower()
+total_letters = len(name.replace(" ", ""))
+first_name_letters = len(name.split()[0])
+
+print(uppercase_name)
+print(lowercase_name)
+print(total_letters)
+print(first_name_letters)
